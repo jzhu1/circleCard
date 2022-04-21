32a5121159124e0d39e6eb7abab69b847d060b83
jzhu1 <21694479+jzhu1@users.noreply.github.com>
2022-04-21 15:39:16 -0400

    init

---
+++ .tmp/build/.tmp/precompile/visualPlugin.d.ts
@@ -0,0 +1,4 @@
+import powerbiVisualsApi from "powerbi-visuals-api";
+import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
+declare var circleCardD174996480274E5C8BACB3B94487468E: IVisualPlugin;
+export default circleCardD174996480274E5C8BACB3B94487468E;
